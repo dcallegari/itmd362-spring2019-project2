9855345ea5b46cac45b97b3260798e00de3f8ba1
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-04-06 16:31:21 -0500

    Modified the mobile nav bar to have slidable submenus

--- js/index_script.js
+++ js/index_script.js
@@ -10,68 +10,57 @@
 // Runs when the document starts
 $(document).ready(function() {
 
-});
+  // ======== Toggles nav menu dropdowns ========
+  $("#menu").click(function() {
+    $("#navbars").slideToggle();
+  });
 
-
-// =====================================================================
-// Toggles nav menu dropdowns
-$("#menu").click(function() {
-  if($("#navbars").is(":hidden")){
-    $("#navbars").slideDown();
-  } else {
-    $("#navbars").slideUp();
-  }
-});
-
-$("#contactbtn").click(function() {
-  if($("#contactdropdown").is(":hidden")){
-    $("#contactdropdown").slideDown();
-  } else {
-    $("#contactdropdown").slideUp();
-  }
-});
-
-$("#eventsbtn").click(function() {
-  if($("#eventsdropdown").is(":hidden")){
-    $("#eventsdropdown").slideDown();
-  } else {
+  $("#contactbtn").click(function() {
     $("#eventsdropdown").slideUp();
-  }
-});
-
-$("#aboutusbtn").click(function() {
-  if($("#aboutusdropdown").is(":hidden")){
-    $("#aboutusdropdown").slideDown();
-  } else {
     $("#aboutusdropdown").slideUp();
-  }
+    $("#homedropdown").slideUp();
+    $("#contactdropdown").slideToggle();
+  });
+
+  $("#eventsbtn").click(function() {
+    $("#contactdropdown").slideUp();
+    $("#aboutusdropdown").slideUp();
+    $("#homedropdown").slideUp();
+    $("#eventsdropdown").slideToggle();
+  });
+
+  $("#aboutusbtn").click(function() {
+    $("#contactdropdown").slideUp();
+    $("#eventsdropdown").slideUp();
+    $("#homedropdown").slideUp();
+    $("#aboutusdropdown").slideToggle();
+  });
+
+  $("#homebtn").click(function() {
+    $("#contactdropdown").slideUp();
+    $("#eventsdropdown").slideUp();
+    $("#aboutusdropdown").slideUp();
+    $("#homedropdown").slideToggle();
+  });
+
 });
 
-$("#homebtn").click(function() {
-  if($("#homedropdown").is(":hidden")){
-    $("#homedropdown").slideDown();
-  } else {
-    $("#homedropdown").slideUp();
-  }
-});
+
 
 
 // =====================================================================
-// Enables navbar when screen is bigger than 960px
-// Hides the mobile navbar when resizing from big to small
+// Shows or hides the mobile navbar when resizing to larger screen
 var resized = false;
-
 $(window).resize(function(){
-  //whole navbar
-  if ($(window).innerWidth >= 960){
-    $("#navbars").slideDown(0);
+  if ($(window).outerWidth() >= 960){
+    $("#navbars").show();
     resized = true;
   } else if (resized == true){
-    $("#navbars").slideUp(0);
-    $("#contactdropdown").slideUp(0);
-    $("#eventsdropdown").slideUp(0);
-    $("#aboutusdropdown").slideUp(0);
-    $("#homedropdown").slideUp(0);
+    $("#navbars").hide();
+    $("#contactdropdown").hide();
+    $("#eventsdropdown").hide();
+    $("#aboutusdropdown").hide();
+    $("#homedropdown").hide();
     resized = false;
   }
 });
