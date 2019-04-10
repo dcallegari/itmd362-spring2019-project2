d15aeb404065981f9b6554e5bcbcf0da25ae1f35
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-04-10 16:32:00 -0500

    Added all the content to signup form

--- js/signupform_script.js
+++ js/signupform_script.js
@@ -1,16 +1,17 @@
 /* 
    Javascript Program to add functionality to homepage.    
    Programmers: Daniel Callegari, Dongboum Shin, Jacob Huebner
-   Date created: 4/8/2019
-   Date modified: 4/8/2019
-   File Name: custom_script.js
+   Date created: 4/10/2019
+   Date modified: 4/10/2019
+   File Name: signupform_script.js
 */
 
 // Runs when the document starts
 $(document).ready(function() {
 
-  $("#btnSubmit").click(function(){
-        alert("button");
-  })
+  $(this).submit(function(){
+    alert("Thank you for signing up. You will be redirected back to the homepage in a moment...");
+    $( "#homebtn" ).trigger( "click" );
+});
 
 });
