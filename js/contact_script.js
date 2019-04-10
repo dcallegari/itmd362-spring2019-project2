23731aa39e2b5fb0fbc2b3871bac61f7f2a2222f
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-04-10 16:55:27 -0500

    Added contact page

--- js/contact_script.js
+++ js/contact_script.js
@@ -1,7 +1,17 @@
 /* 
-   Javascript Program to add functionality to contact page.
+   Javascript Program to add functionality to homepage.    
    Programmers: Daniel Callegari, Dongboum Shin, Jacob Huebner
-   Date created: 4/5/2019
-   Date modified: 4/5/2019
+   Date created: 4/10/2019
+   Date modified: 4/10/2019
    File Name: contact_script.js
 */
+
+// Runs when the document starts
+$(document).ready(function() {
+
+  $(this).submit(function(){
+    alert("Message sent! We thank you for your input.");
+    return false;
+});
+
+});
